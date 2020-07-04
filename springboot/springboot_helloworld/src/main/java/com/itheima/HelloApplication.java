fe1223c1d8fe5a294498ecf363ff1e41f6837c4a
JoinSheng <zscaoc@isoftstone.com>
2020-07-05 01:58:34 +0800

    第二次提交,修改姓名

--- springboot/springboot_helloworld/src/main/java/com/itheima/HelloApplication.java
+++ springboot/springboot_helloworld/src/main/java/com/itheima/HelloApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @Author: JoinCao
+ * @Author: JoinSheng
  * @CreateDate: 2020-02-29
  */
 @SpringBootApplication//表示引导类,SpringBoot项目入口
